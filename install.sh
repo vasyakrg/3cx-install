bf29cdd9dd93c0065de1148b4f1bd0fa9dcff4b5
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-04-24 16:40:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 apt update
 
-apt install -y wget curl
+apt install -y wget
 
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add -
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
