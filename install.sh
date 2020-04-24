04e87753f0ebe0d7235a0e9a3d9c62cbd3f65836
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-04-24 16:39:21 +0700

    Update install.sh
    without questions

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 apt update
 
-apt install wget curl
+apt install -y wget curl
 
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add -
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch-testing main" | sudo tee /etc/apt/sources.list.d/3cxpbx-testing.list
 sudo apt update
-sudo apt install net-tools dphys-swapfile
-sudo apt install 3cxpbx
+sudo apt install -y net-tools dphys-swapfile
+sudo apt install -y 3cxpbx
